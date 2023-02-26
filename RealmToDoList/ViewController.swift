340b106e76d9af60bd8b11e71fefa59f79e3eedf
AntAbramov <ant.itdev@icloud.com>
2023-02-26 16:17:40 +0300

    Initial Commit

---
+++ RealmToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmToDoList
+//
+//  Created by Anton Abramov on 26.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
